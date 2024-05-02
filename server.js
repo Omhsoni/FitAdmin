471361612e46d846ec01680943945ce2d87505b8
Om Soni <123151714+Omhsoni@users.noreply.github.com>
2024-05-02 18:29:43 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express"); 
+const app = express();
+const mongoose = require("mongoose");
+const User = require("./models/user.js")
+
+const URL = "mongodb://127.0.0.1:27017/FitAdmin";
+
+main().then( () => {
+    console.log("Server connected to database.");
+})
+.catch((err) => {
+    console.log(err)
+});
+
+async function main() {
+    await mongoose.connect(URL);
+}
+
+app.get("/", (req,res) => {
+    res.send("This is root of FitAdmin");
+});
+
+app.listen(4080, () =>{
+    console.log("server is ready and listening");
+});
